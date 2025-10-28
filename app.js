d3f91556fac13ffc04721e2ba8c1fd9f3f8c45cf
f00d <delossantos.orion@student.greenriver.edu>
2025-10-28 11:47:42 -0700

    adding post

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ import express from 'express';
 
 const app = express();
 const PORT = 3001;
+const order = [];
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
 app.get('/', (req,res) =>{
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+    res.render('home');
 
 });
 
 app.listen(PORT,() =>{
-    // console.log(`Server is running at http://localhost:${PORT}`)
-    console.log("Server is running at http://137.184.41.43:3001")
+    console.log(`Server is running at http://localhost:${PORT}`)
+    //console.log("Server is running at http://137.184.41.43:3001")
 })
